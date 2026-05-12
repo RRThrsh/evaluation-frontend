02606b5f5b2b513e897a0980809b1cc28664cc51
Yhaanxi <RRThrsh@gmail.com>
2026-05-12 15:51:30 +0800

    FIX: app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 export default function App() {
-    return <h1>Hello Vite + React 🚀</h1>
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
+    );
 }

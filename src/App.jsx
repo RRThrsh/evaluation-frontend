0f9c87c755053f929af7cbb01dd367e1621590c6
Yhaanxi <RRThrsh@gmail.com>
2026-05-12 17:13:08 +0800

    FIX: import not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
+
+{/* PAGES */}
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import NotFound from "./pages/NotFound";
 
 export default function App() {
     return (
         <Routes>
+            {/* PUBLIC ROUTES */}
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+
+            {/* ERROR HANDLER */}
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }

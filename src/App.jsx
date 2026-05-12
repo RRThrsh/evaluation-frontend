95aace2e8656bf2555fae9537c2a3cd73376ceff
Yhaanxi <RRThrsh@gmail.com>
2026-05-12 22:38:19 +0800

    FIX: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,64 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-{/* PAGES */}
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+// PAGES
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import TooManyRequests from "./pages/TooManyRequests";
-import Home from './pages/Home';
 
-{/* ROUTES */}
+// ROUTES
 import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
     return (
         <Routes>
-            {/* PUBLIC ROUTES */}
+
+            {/* PUBLIC */}
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/home" element={<Home />} />
 
-            {/* USERS */}
-            <Route path="/dashboard" element={
-                <ProtectedRoute allowedRoles={["user", "moderator", "admin"]}>
-                    <div>Dashboard</div>
-                </ProtectedRoute>
-            } />
+            {/* USER */}
+            <Route
+                path="/dashboard"
+                element={
+                    <ProtectedRoute
+                        allowedRoles={["user", "moderator", "admin"]}
+                    >
+                        <div>Dashboard</div>
+                    </ProtectedRoute>
+                }
+            />
 
-            {/* MODERATORS */}
-            <Route path="/moderate" element={
-                <ProtectedRoute allowedRoles={["moderator", "admin"]}>
-                    <div>Moderation Panel</div>
-                </ProtectedRoute>
-            } />
+            {/* MODERATOR */}
+            <Route
+                path="/moderate"
+                element={
+                    <ProtectedRoute
+                        allowedRoles={["moderator", "admin"]}
+                    >
+                        <div>Moderation Panel</div>
+                    </ProtectedRoute>
+                }
+            />
 
             {/* ADMIN */}
-            <Route path="/admin" element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <div>Admin Panel</div>
-                </ProtectedRoute>
-            } />
+            <Route
+                path="/admin"
+                element={
+                    <ProtectedRoute allowedRoles={["admin"]}>
+                        <div>Admin Panel</div>
+                    </ProtectedRoute>
+                }
+            />
 
-            {/* ERROR HANDLER */}
+            {/* ERRORS */}
             <Route path="/429" element={<TooManyRequests />} />
             <Route path="*" element={<NotFound />} />
+
         </Routes>
     );
 }

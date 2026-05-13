b6586fac684a8b650e7994fc5562ae436be17fe2
Yhaanxi <RRThrsh@gmail.com>
2026-05-13 19:05:38 +0800

    FEAT: badge style

---
+++ src/components/common/badge/badge.style.js
@@ -0,0 +1,6 @@
+export const BADGE_STYLES = {
+    default: "px-2 py-1 text-xs rounded bg-gray-200 text-gray-800",
+    success: "px-2 py-1 text-xs rounded bg-green-200 text-green-800",
+    warning: "px-2 py-1 text-xs rounded bg-yellow-200 text-yellow-800",
+    error: "px-2 py-1 text-xs rounded bg-red-200 text-red-800",
+};
